cb3b7a02f7d1bfc7e786c39a7017abf2cea05fac
Tanveer Wahid <twahid@keeta.com>
2025-08-26 17:34:16 -0700

    Build relative paths for generated modules.

--- asn1/generated/algorithm_identifier.rs
+++ asn1/generated/algorithm_identifier.rs
@@ -1,10 +1,10 @@
-#[allow(unused_imports)]
-#[allow(unused_variables)]
-#[allow(dead_code)]
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(non_upper_case_globals)]
-#[allow(clippy::all)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(clippy::all)]
 use rasn::prelude::*;
 #[derive(AsnType, Debug, Clone, Decode, Encode, PartialEq, Eq, Hash)]
 pub struct AlgorithmIdentifier {
--- asn1/generated/subject_public_key_info.rs
+++ asn1/generated/subject_public_key_info.rs
@@ -1,11 +1,11 @@
-#[allow(unused_imports)]
-#[allow(unused_variables)]
-#[allow(dead_code)]
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(non_upper_case_globals)]
-#[allow(clippy::all)]
-use super::algorithm_identifier::AlgorithmIdentifier;
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(clippy::all)]
+use super::algorithm_identifier_definitions::AlgorithmIdentifier;
 use rasn::prelude::*;
 #[derive(AsnType, Debug, Clone, Decode, Encode, PartialEq, Eq, Hash)]
 pub struct SubjectPublicKeyInfo {
